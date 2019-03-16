5634a17565fafc26bd0c236cb8dbecd90bb35d9d
jinka <voiphandy@gmail.com>
2019-03-16 18:45:38 +0300

    Django installed and virtual env

---
+++ venvawwards/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/daud/Desktop/moringa-school/Core/Python/Django/WEEK3-IP/venvawwards/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

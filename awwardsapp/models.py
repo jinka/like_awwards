a3cc76850a17ab9af7809a599f7954f5a5737d60
jinka <voiphandy@gmail.com>
2019-03-18 04:57:16 +0300

    Database migrations done after createing Project model

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -8,7 +8,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg',upload_to='profile_pics')
     bio = models.TextField(max_length=500, blank=True)
-    rate = models.ManyToManyField('Project', related_name='image',max_length=30)
     contact = models.TextField(max_length=500, blank=True)
 
 
@@ -28,9 +27,8 @@ class Project(models.Model):
     title = models.CharField(max_length =10)
     url = models.CharField(max_length =50)
     detail_desciption=models.TextField(max_length=500)
-    profile = models.ForeignKey(Profile, null = True,related_name='project')
-    pub_date = models.DateTimeField(auto_now_add=True, null=True)
-    user= models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, related_name='posted_by', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['-pk']
